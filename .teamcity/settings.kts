832790bacfdf41d88432b7ec2f4db04c00aab34e
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2026-05-26 08:48:24 +0100

    DSL change: streamline the build configs

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.buildFeatures.pullRequests
@@ -6,43 +7,26 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.buildSteps.kotlinFile
 import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.version
 
-version = "2025.03"
+version = "2026.1"
 
 project {
-    vcsRoot(TagReleaseVcs)
-    vcsRoot(PullRequestVcs)
-    vcsRoot(MasterVcs)
-
     buildType(ReleaseBuildConfiguration)
-    buildType(PullRequestBuildConfiguration)
     buildType(MasterBuildConfiguration)
 }
 
-object TagReleaseVcs : GitVcsRoot({
-    id("TagReleaseVcs")
-    name = "TagReleaseVcs"
-    branch = "master"
-    useTagsAsBranches = true
-    branchSpec = """
-        +:refs/(tags/*)
-        -:<default>
-    """.trimIndent()
-    url = "https://github.com/JetBrains/teamcity-unreal-engine-plugin.git"
-})
-
 object ReleaseBuildConfiguration : BuildType({
     id("ReleaseBuild")
-    name = "ReleaseBuild"
+    name = "Unreal Plugin: release build"
 
     params {
         password("env.ORG_GRADLE_PROJECT_jetbrains.marketplace.token", "credentialsJSON:f5d30123-5324-4032-b9d0-aa8b5bf6c6d5", readOnly = true)
     }
 
     vcs {
-        root(TagReleaseVcs)
+        root(DslContext.settingsRoot)
+        branchFilter = "+:tags/*"
     }
 
     steps {
@@ -74,41 +58,30 @@ object ReleaseBuildConfiguration : BuildType({
     }
 })
 
-object PullRequestVcs : GitVcsRoot({
-    id("PullRequestVcs")
-    name = "PullRequestVcs"
-    branch = "refs/heads/master"
-    branchSpec = """
-        -:<default>
-    """.trimIndent()
-    url = "https://github.com/JetBrains/teamcity-unreal-engine-plugin.git"
-})
+object MasterBuildConfiguration : BuildType({
+    id("MasterBuild")
+    name = "Unreal Plugin: master and PRs build"
 
-object PullRequestBuildConfiguration : BuildType({
-    id("PullRequestBuild")
-    name = "PullRequestBuild"
+    allowExternalStatus = true
 
     val githubTokenParameter = "GITHUB_TOKEN"
     params {
-        password(githubTokenParameter, "credentialsJSON:9836dc1a-9546-4018-b7c5-b5cbbaa03213", readOnly = true)
+        password(githubTokenParameter, "credentialsJSON:9836dcgi1a-9546-4018-b7c5-b5cbbaa03213", readOnly = true)
     }
 
     vcs {
-        root(PullRequestVcs)
+        root(DslContext.settingsRoot)
     }
 
     triggers {
         vcs {
-            branchFilter = """
-                +:*
-                -:<default>
-            """.trimIndent()
+            branchFilter = "+:*"
         }
     }
 
     features {
         pullRequests {
-            vcsRootExtId = PullRequestVcs.id?.value
+            vcsRootExtId = DslContext.settingsRoot.id?.value
             provider = github {
                 filterTargetBranch = "refs/heads/master"
                 filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER_OR_COLLABORATOR
@@ -118,48 +91,13 @@ object PullRequestBuildConfiguration : BuildType({
             }
         }
         commitStatusPublisher {
-            vcsRootExtId = PullRequestVcs.id?.value
+            vcsRootExtId = DslContext.settingsRoot.id?.value
             publisher = github {
                 githubUrl = "https://api.github.com"
                 authType = personalToken {
                     token = "%$githubTokenParameter%"
                 }
             }
-        }
-    }
-
-    steps {
-        gradle {
-            name = "build"
-            tasks = "clean build serverPlugin"
-        }
-    }
-
-    requirements {
-        exists("env.JDK_11")
-    }
-})
-
-object MasterVcs : GitVcsRoot({
-    id("MasterVcs")
-    name = "MasterVcs"
-    branch = "refs/heads/master"
-    url = "https://github.com/JetBrains/teamcity-unreal-engine-plugin.git"
-})
-
-object MasterBuildConfiguration : BuildType({
-    id("MasterBuild")
-    name = "MasterBuild"
-
-    allowExternalStatus = true
-
-    vcs {
-        root(MasterVcs)
-    }
-
-    triggers {
-        vcs {
-            branchFilter = "+:*"
         }
     }
 
